6821a44a7fb805d81664313318bf8d666e49b24a
Warren <warrener.ryan@gmail.com>
2026-08-30 14:16:46 -0400

    Updated Inventory Menu

--- InventoryMenu.gd
+++ InventoryMenu.gd
@@ -3,7 +3,9 @@ extends CanvasLayer
 @onready var item_rect = $MainSplit/RightMenu/ContentArea/ItemRect
 @onready var weapon_rect = $MainSplit/RightMenu/ContentArea/WeaponRect
 @onready var status_image = $MainSplit/RightMenu/RightLowerMenu/StatusImage
+@onready var health_overlay = $MainSplit/RightMenu/RightLowerMenu/StatusImage/HealthOverlay
 
+var current_health: int = 100
 
 func _ready():
 	hide()
@@ -18,10 +20,14 @@ func _input(event):
 			open_inventory()
 			
 func open_inventory():
+	await RenderingServer.frame_post_draw
+	
 	var current_frame = get_viewport().get_texture().get_image()
 	var frame_texture = ImageTexture.create_from_image(current_frame)
-	
 	status_image.texture = frame_texture
+	
+	update_health_colour()
+	
 	get_tree().paused = true
 	show()
 
@@ -32,7 +38,13 @@ func close_inventory():
 	item_rect.hide()
 	weapon_rect.hide()
 	
-
+func update_health_colour():
+	if current_health > 70:
+		health_overlay.modulate = Color(0.2,0.8,0.2,0.6)
+	elif current_health > 30:
+		health_overlay.modulate = Color(0.8,0.8,0.2,0.6)
+	else:
+		health_overlay.modulate = Color(0.8,0.2,0.2,0.6)
 func _on_items_btn_pressed():
 	item_rect.show()
 	weapon_rect.hide()
